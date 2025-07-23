46a66c0050eb393c82a4b4ac86b7f1eed734e680
eder1011 <106632305+eder1011@users.noreply.github.com>
2025-07-23 00:10:51 -0300

    Update exercicio_4.py

--- exercicio_4.py
+++ exercicio_4.py
@@ -6,8 +6,16 @@ só com os números primos.
 
 Mas você não precisa escrever a lógica para saber se o número é primo: você pode só usar a funcão
 `verificar_se_eh_primo` disponibilizada, que vai retornar True se o número for primo e False se não for.
+
 """
 
-
 def filtrar_primos(lista):
-    return [numero for numero in lista if verificar_se_eh_primo(numero)]
+   
+    if not isinstance(lista, list):
+        raise TypeError("A entrada deve ser uma lista.")
+
+    primos = []
+    for numero in lista:
+        if verificar_se_eh_primo(numero):
+            primos.append(numero)
+    return primos
